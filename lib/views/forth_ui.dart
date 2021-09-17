e71fd4d5302a4bec6d285d172c89fac6680104f7
s6252410019 <s6252410019@sau.ac.th>
2021-09-17 07:37:27 +0700

    Code V.2 : Do YYYY

---
+++ lib/views/forth_ui.dart
@@ -0,0 +1,28 @@
+
+
+import 'package:flutter/material.dart';
+
+class ForthUI extends StatefulWidget {
+  const ForthUI({ Key? key }) : super(key: key);
+
+  @override
+  _ForthUIState createState() => _ForthUIState();
+}
+
+class _ForthUIState extends State<ForthUI> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.pink,
+        title: Text(
+          'Forth UI ไอที',
+          style: TextStyle(
+            fontFamily: 'Kanit',
+          ),
+        ),
+        centerTitle: true,
+      ),
+    );
+  }
+}
